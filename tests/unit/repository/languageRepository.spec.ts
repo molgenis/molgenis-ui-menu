b36c2ca1a2e8b99aa1c5ee632714c561ca2a1118
connor <c.stroomberg@gmail.com>
2019-11-11 22:16:18 +0100

    bump test cov

---
+++ tests/unit/repository/languageRepository.spec.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import api from '@molgenis/molgenis-api-client'
+import languageRepository from '../../../src/repository/LanguageRepository'
+
+describe('LanguageRepository', () => {
+  describe('setSelectedLanguage', () => {
+    const spy = jest.spyOn(api, 'post')
+    beforeEach(async(done) => {
+      await languageRepository.setSelectedLanguage('fr')
+      done()
+    })
+    it('should use the api to set the given language code', () => {
+      expect(spy).toBeCalledWith('/plugin/useraccount/language/update?languageCode=fr')
+    })
+  })
+})
