44c1d7fccf057a543578c7eede267969b0049965
Fleur Kelpin <f.kelpin@umcg.nl>
2019-11-18 11:30:49 +0100

    test(store): test getters

--- tests/unit/store/getters.spec.ts
+++ tests/unit/store/getters.spec.ts
@@ -1,7 +1,7 @@
-import { molgenisMenu } from '@/store/getters'
+import { molgenisMenu, molgenisFooter } from '@/store/getters'
 import context from '../fixtures/context'
 import state from '@/store/state'
-import { Context } from '@/types'
+import { Context, ContextState, MolgenisMenu } from '@/types'
 
 describe('getters', () => {
   describe('molgenisMenu', () => {
@@ -30,6 +30,34 @@ describe('getters', () => {
         navBarLogo: "/img/Logo_Blue_Small.png",
         topLogoMaxHeight: 150
       })
+    })
+
+    it('can maps logoTop from context', () => {
+      const actual = molgenisMenu({context: {...context, logoTop: 'https://example.org/logotop.jpg'}})
+      expect(actual && actual.topLogo).toEqual('https://example.org/logotop.jpg')
+    })
+  })
+
+  describe('molgenisFooter', () => {
+    it('maps null context to null', () => {
+      const actual = molgenisFooter(state)
+      expect(actual).toBeNull()
+    })
+
+    it('maps context', () => {
+      const actual = molgenisFooter({context})
+      expect(actual).toEqual({
+        additionalMessage: "Footerdefooter",
+        buildDate: "2019-11-07 15:05 UTC",
+        "molgenisSite": "https://www.molgenis.org",
+        "version": "8.3.0-SNAPSHOT"
+      })
+    })
+    it('can handle missing additional message in context', () => {
+      const contextCopy: Context = {...context}
+      delete contextCopy.additionalMessage
+      const actual = molgenisFooter({context: contextCopy})
+      expect(actual!=null && actual.hasOwnProperty('additionalMessage')).toBeFalsy()
     })
   })
 })
